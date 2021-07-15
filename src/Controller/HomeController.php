0aea6c487aa979d31f145fbb06bbdbdca57c5d9e
espirito237 <adouessono@yahoo.fr>
2021-07-15 16:21:25 +0200

    mise en place du formulaire de contact sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, \Swift_Mailer $mailer): Response
     {
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+
+            // On crée le message
+            $message = (new \Swift_Message('Nouveau contact'))
+                // On attribue l'expéditeur
+                ->setFrom($contact['email'])
+                // On attribue le destinataire
+                ->setTo('essonoadou@gmail.com')
+                // On crée le texte avec la vue
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig', compact('contact')
+                    ),'text/html'
+                )
+            ;
+            
+            $mailer->send($message);
+
+            $this->addFlash('success-message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); 
+                    
+            return $this->redirectToRoute('home');
+        }   
+        
         return $this->render('home/index.html.twig', [
             
+        "form" => $form->createView()
         ]);
     }
+
 }
