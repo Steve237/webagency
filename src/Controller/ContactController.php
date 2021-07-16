45d52f1462cdbb4cc8d5343422073084e75df03b
espirito237 <adouessono@yahoo.fr>
2021-07-16 09:46:33 +0200

    mise en service du formulaire de contact dans la page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\SubmitProjectType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, \Swift_Mailer $mailer): Response
     {
+
+        $form = $this->createForm(SubmitProjectType::class);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+
+            // On crée le message
+            $message = (new \Swift_Message('Nouveau projet'))
+                // On attribue l'expéditeur
+                ->setFrom($contact['email'])
+                // On attribue le destinataire
+                ->setTo('essonoadou@gmail.com')
+                // On crée le texte avec la vue
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact-projet.html.twig', compact('contact')
+                    ),'text/html'
+                )
+            ;
+            
+            $mailer->send($message);
+
+            $this->addFlash('success-message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); 
+                    
+            return $this->redirectToRoute('contact');
+
+        }
+
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/SubmitProjectType.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class SubmitProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name', TextType::class, [
+
+            "label" => false,
+
+            "attr" => [
+
+                "class" => "form-control input-width",
+
+                "placeholder" => "Veuillez entrer votre nom",
+                "require" => true
+            ]
+        ])
+
+
+        ->add('email', EmailType::class, [
+
+            "label" => false,
+
+            "attr" => [
+
+                "class" => "form-control input-width",
+                "placeholder" => "Veuillez entrer votre adresse email",
+                "require" => true
+            ]
+        ])
+
+
+        ->add('message', TextareaType::class, [
+
+            "label" => false,
+
+            "attr" => [
+
+                "class" => "form-control input-width",
+                "placeholder" => "Veuillez entrer votre message",
+                "cols" => 30,
+                "rows" => 10,
+                "require" => true
+            ]
+        ])
+
+        ->add('tel', TelType::class, [
+
+            "label" => false,
+
+            "attr" => [
+
+                "class" => "form-control input-width",
+                "placeholder" => "Veuillez entrer votre numéro de téléphone",
+                "require" => true
+            ]
+        ])
+
+
+        ->add('society', TextType::class, [
+
+            "label" => false,
+
+            "attr" => [
+
+                "class" => "form-control input-width",
+                "placeholder" => "Veuillez entrer le nom de votre société",
+                "require" => true
+            ]
+        ])
+
+    ;
+    
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
