99801c0c000266e16b7ba7ec59cec194bff8e468
espirito237 <adouessono@yahoo.fr>
2021-07-28 12:46:25 +0200

    ajout de la pagination dans la liste des projets

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Repository\ProjectRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +15,16 @@ class PortfolioController extends AbstractController
     /**
      * @Route("/portfolio", name="portfolio")
      */
-    public function index(ProjectRepository $projectRepo): Response
+    public function index(ProjectRepository $projectRepo, PaginatorInterface $paginator, Request $request): Response
     {
-        $projects = $projectRepo->findAll();
+
+        $projectsList = $projectRepo->findAll();
+
+        $projects = $paginator->paginate(
+            $projectsList, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            20 /*limit per page*/
+        );
 
         return $this->render('portfolio/portfolio.html.twig', [
             
